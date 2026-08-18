5a640557945c0bdb50637d344173c681501f9f6c
Daniel Agbay <dagbay@redhat.com>
2026-08-18 10:27:09 -0500

    linter fixes

--- test/e2e/hermetic_authz_test.go
+++ test/e2e/hermetic_authz_test.go
@@ -199,10 +199,10 @@ func TestHermeticAuthzCheckCertAuth(t *testing.T) {
 		[]trust.CredentialType{trust.CredentialTypeForwardedClientCert},
 		trust.WithLuaHTTPClient(httpClient),
 		trust.WithLuaConfigSource(luaservices.NewMapConfigSource(map[string]any{
-			"bop_url":              bopURL,
-			"bop_env":              "stage",
-			"trust_domain":         "cert.example.com",
-			"bop_certauth_secret":  "test-secret",
+			"bop_url":             bopURL,
+			"bop_env":             "stage",
+			"trust_domain":        "cert.example.com",
+			"bop_certauth_secret": "test-secret",
 		})),
 	)
 	if err != nil {
